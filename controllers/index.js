2b315815c865250c44be6835a6b00eca16d0c4d4
Jay Walker <jwalker.nationwide@gmail.com>
2017-12-04 21:18:27 -0500

    added graphql

---
+++ controllers/index.js
@@ -0,0 +1,1 @@
+var categories = require('./applications');
--- core/server.js
+++ core/server.js
@@ -2,7 +2,7 @@ const  express  =  require('express');
 const  graphqlHTTP  =  require('express-graphql'); 
 const  app  =  express(); 
 
-var ctlr = require('../controllers/applications');
+var ctlr = require('../controllers')
 var settings = require('../settings');
 
 var port = process.env.PORT || settings.webPort;

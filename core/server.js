dc34e29a49f037202acd518c7b2a030c87a0a928
Jay Walker <jwalker.nationwide@gmail.com>
2017-12-04 20:29:31 -0500

    added some new modules

---
+++ core/server.js
@@ -0,0 +1,15 @@
+const  express  =  require('express');
+const  graphqlHTTP  =  require('express-graphql'); 
+const  app  =  express(); 
+
+var ctlr = require('../controllers/applications');
+var settings = require('../settings');
+
+var port = process.env.PORT || settings.webPort;
+
+app.use('/graphql',  graphqlHTTP({  
+    schema:  MyGraphQLSchema,
+      graphiql:  true
+})); 
+
+app.listen(port);
---
+++ settings.js
@@ -0,0 +1,8 @@
+exports.mysqlConn = {
+    host: 'localhost',
+    user: 'walkerja',
+    password: 'jcasual1',
+    database: 'maintain',
+};
+
+exports.webPort = 3000;
